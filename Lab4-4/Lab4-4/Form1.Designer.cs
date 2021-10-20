6c29b52feaa4d43bb9c86216ddb8f61472db5cc3
Sioldil <61321409+Sioldil@users.noreply.github.com>
2021-10-20 12:00:28 +0700

    Add Lab04

---
+++ Lab4-4/Lab4-4/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace Lab4_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvOrder = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpTo = new System.Windows.Forms.DateTimePicker();
+            this.dtpFrom = new System.Windows.Forms.DateTimePicker();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrder)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvOrder
+            // 
+            this.dgvOrder.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvOrder.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dgvOrder.Location = new System.Drawing.Point(12, 116);
+            this.dgvOrder.Name = "dgvOrder";
+            this.dgvOrder.Size = new System.Drawing.Size(776, 324);
+            this.dgvOrder.TabIndex = 3;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "STT";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Số HĐ";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Ngày Đặt Hàng";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Ngày Giao Hàng";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Thành Tiền";
+            this.Column5.Name = "Column5";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.dtpTo);
+            this.groupBox1.Controls.Add(this.dtpFrom);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông TIn Đơn Hàng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(244, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "~";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Thời gian giao hàng";
+            // 
+            // dtpTo
+            // 
+            this.dtpTo.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpTo.Location = new System.Drawing.Point(264, 62);
+            this.dtpTo.Name = "dtpTo";
+            this.dtpTo.Size = new System.Drawing.Size(80, 20);
+            this.dtpTo.TabIndex = 1;
+            this.dtpTo.ValueChanged += new System.EventHandler(this.dtpFrom_ValueChanged);
+            // 
+            // dtpFrom
+            // 
+            this.dtpFrom.CustomFormat = "MM/yyyy";
+            this.dtpFrom.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFrom.Location = new System.Drawing.Point(158, 62);
+            this.dtpFrom.Name = "dtpFrom";
+            this.dtpFrom.Size = new System.Drawing.Size(80, 20);
+            this.dtpFrom.TabIndex = 1;
+            this.dtpFrom.ValueChanged += new System.EventHandler(this.dtpFrom_ValueChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(28, 20);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(134, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Xem tất cả trong tháng";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvOrder);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrder)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvOrder;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpTo;
+        private System.Windows.Forms.DateTimePicker dtpFrom;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Lab4-4/Lab4-4/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Lab4_4.Models;
+
+namespace Lab4_4
+{
+    public partial class Form1 : Form
+    {
+        ProductDB Context = new ProductDB();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region Method
+        private void loadGridView(List<Order> listOders)
+        {
+            dgvOrder.Rows.Clear();
+            List<Order> List = listOders.Where(p => p.Invoice.DeliveryDate >= dtpFrom.Value && p.Invoice.DeliveryDate <= dtpTo.Value).ToList();
+            var List2 = from q in List
+                        group q by new { q.InvoiceNo, q.Invoice.OrderDate, q.Invoice.DeliveryDate} into p
+                        select new { InvoiceNo = p.Key.InvoiceNo, OrderDate = p.Key.OrderDate, p.Key.DeliveryDate, Price = p.Sum(o => o.Price * o.Quantity) };
+            foreach (var item in List2)
+            {
+                int index = dgvOrder.Rows.Add();
+
+                for(int i = 0; i< dgvOrder.Rows.Count ; i++)
+
+                dgvOrder.Rows[index].Cells[0].Value = i;
+                dgvOrder.Rows[index].Cells[1].Value = item.InvoiceNo;
+                dgvOrder.Rows[index].Cells[2].Value = item.OrderDate.ToString("dd/MM/yyyy");
+                dgvOrder.Rows[index].Cells[3].Value = item.DeliveryDate.Date.ToString("dd/MM/yyyy");
+                dgvOrder.Rows[index].Cells[4].Value = item.Price;
+            }
+        }
+
+        public void SetMyCustomFormat()
+        {
+            // Set the Format type and the CustomFormat string.
+            dtpFrom.Format = DateTimePickerFormat.Custom;
+            dtpFrom.CustomFormat = "dd/MM/yyyy";
+            dtpTo.Format = DateTimePickerFormat.Custom;
+            dtpTo.CustomFormat = "dd/MM/yyyy";
+        }
+
+        private void checkBox1_CheckStateChanged()
+        {
+            var M = dtpFrom.Value.Month;
+            var Y = dtpFrom.Value.Year;
+            dtpFrom.Value = new DateTime(Y, M, 01);
+            if (M == 1 || M == 3 || M == 5 || M == 7 || M == 8 || M == 10 || M == 12)
+            {
+                dtpTo.Value = new DateTime(Y, M, 31);
+            }
+            else if (M == 4 || M == 6 || M == 9 || M == 11)
+            {
+                dtpTo.Value = new DateTime(Y, M, 30);
+            }
+            else
+            {
+                dtpTo.Value = new DateTime(Y, M, 28);
+            }
+        }
+        #endregion
+
+        #region Event
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dtpFrom.Value = new DateTime(2019, 10, 01);
+            dtpTo.Value = new DateTime(2019, 10, 03);
+            SetMyCustomFormat();
+            List<Order> listOrders = Context.Orders.ToList();
+            loadGridView(listOrders);        
+        }
+
+        private void dtpFrom_ValueChanged(object sender, EventArgs e)
+        {
+            List<Order> listStudents = Context.Orders.ToList();
+            loadGridView(listStudents);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                checkBox1_CheckStateChanged();
+            }
+        }
+
+        #endregion
+
+
+    }
+}
